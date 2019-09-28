72be240e8013d76ac4ea53823b5b2fda1c5e73c1
Edward <onfe@users.noreply.github.com>
2019-09-28 01:45:07 +0100

    Add header.

---
+++ components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <nav class="container">
+      <n-link to="/" class="onfe">
+        ONFE
+      </n-link>
+      <div class="collapse">
+        <n-link to="blog">Blog</n-link>
+        <n-link to="projects">Projects</n-link>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  width: 100%;
+  padding: $spacer;
+  background: $colour-dark;
+  color: $colour-white;
+}
+
+nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.onfe {
+  position: relative;
+  text-decoration: none;
+  font-size: 1.25em;
+  margin: -0.2em 0;
+  font-family: 'LemonMilk';
+  color: $colour-yellow;
+  transition: all 300ms ease-in-out;
+
+  &:hover {
+    transform: translate(-0.1em, -0.1em);
+    text-shadow: 0.1em 0.1em #fff9, 0.2em 0.2em $colour-blue;
+  }
+}
+</style>

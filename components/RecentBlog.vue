974e4068228da42a27840188c850c01aceac141e
Edward <onfe@users.noreply.github.com>
2019-10-02 01:12:03 +0100

    Add 'RecentBlog' skeleton.

---
+++ components/RecentBlog.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <HeadingMore class="heading" title="Latest Posts" more="/blog" />
+    <div class="pad">
+      <div class="container">
+        <div class="posts card">
+          <div class="card-body">
+            Imagine me... having a BLOG?! seems fake but ok.
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import HeadingMore from '@/components/HeadingMore.vue'
+
+export default {
+  components: {
+    HeadingMore
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.pad {
+  padding: $spacer;
+}
+</style>

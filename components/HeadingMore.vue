311e20d56e03dc846eae3a8736c371f7cf104686
Edward <onfe@users.noreply.github.com>
2019-10-02 01:09:58 +0100

    Add HeadingMore component.

---
+++ components/HeadingMore.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="heading">
+    <div class="container split">
+      <h2 class="">{{ title }}</h2>
+      <n-link :to="more" class="more">more</n-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Untitled'
+    },
+    more: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.split {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+}
+
+.heading {
+  padding: 0 $spacer;
+
+  @include sm {
+    padding: 0;
+  }
+}
+</style>

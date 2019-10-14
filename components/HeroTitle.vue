ef13419280ec8437aabb521d5d240427264f55be
Edward <onfe@users.noreply.github.com>
2019-10-14 21:28:32 +0100

    Add projects.

---
+++ components/HeroTitle.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="titleHero">
+    <div class="container">
+      <h1>{{ title }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.titleHero {
+  width: 100%;
+  background: $colour-dark;
+  color: $colour-light;
+  padding: $spacer;
+}
+
+h1 {
+  font-size: 1.25em;
+  margin: 0;
+
+  @include sm {
+    font-size: 1.5em;
+  }
+}
+</style>
---
+++ pages/projects/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    <HeroTitle title="Projects" />
+    <Completionist />
+  </div>
+</template>
+
+<script>
+import HeroTitle from '@/components/HeroTitle.vue'
+import Completionist from '@/components/projHeros/completionist.vue'
+
+export default {
+  components: {
+    HeroTitle,
+    Completionist
+  }
+}
+</script>

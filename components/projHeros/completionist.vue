a904035e0ffdecb8b316c5020b30d0200ff7e105
Edward <onfe@users.noreply.github.com>
2019-10-14 21:28:47 +0100

    Add completionist.

---
+++ components/projHeros/completionist.vue
@@ -0,0 +1,119 @@
+<template>
+  <section class="proj-hero completionist">
+    <div class="container">
+      <div class="logo">
+        <h2>Completionist</h2>
+      </div>
+      <div class="detail">
+        <div class="card">
+          <div class="card-body">
+            <div class="text">
+              <p>An open-source autocomplete engine, written in C++.</p>
+              <p>Lightning fast. Memory efficient.</p>
+            </div>
+            <a
+              href="https://github.com/onfe/Completionist"
+              class="btn btn-block"
+            >
+              View the Source »
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Raleway:800');
+
+section {
+  background: #0069ff;
+  font-family: 'Raleway';
+  overflow: hidden;
+  padding: 0 $spacer;
+
+  @include sm {
+    background: linear-gradient(90deg, #fff 50%, #0069ff 50%);
+  }
+}
+
+.container {
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  padding: 2 * $spacer 0;
+
+  @include sm {
+    flex-direction: row-reverse;
+    align-items: center;
+    background: linear-gradient(100deg, #fff 25%, #0069ff calc(25% + 1px));
+  }
+}
+
+.logo {
+  flex: 1;
+
+  @include sm {
+    text-align: right;
+    width: 49%;
+    margin: 0;
+  }
+}
+
+.detail {
+  padding-top: $spacer;
+  flex: 1;
+
+  @include sm {
+    padding: 0;
+    padding-right: $spacer;
+    width: 40%;
+  }
+}
+
+.card {
+  background: $colour-white;
+}
+
+h2 {
+  display: inline-block;
+  position: relative;
+  font-size: 2em;
+  font-weight: bolder;
+  margin: 0;
+  padding: 0 0.2em;
+  background: #fff;
+  color: #0069ff;
+  border-radius: 1em 0 0 1em;
+
+  @include md {
+    font-size: 2.75em;
+  }
+
+  @include lg {
+    font-size: 3.5em;
+  }
+}
+
+h2::after {
+  display: block;
+  content: '';
+  position: absolute;
+  left: 99%;
+  width: 100vw;
+  height: 100%;
+  top: 0;
+  background: #fff;
+}
+
+.btn {
+  background: #0069ff;
+  color: $colour-white;
+}
+
+.text p {
+  margin: 0;
+  margin-bottom: 1em;
+}
+</style>

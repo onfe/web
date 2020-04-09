924c3f3f85dafc844b773ce5ff9fe7f3d1d2f547
Edward <onfe@users.noreply.github.com>
2020-04-09 17:40:33 +0100

    Add Blog index.

---
+++ src/pages/Blog.vue
@@ -0,0 +1,62 @@
+<template>
+  <Layout>
+    <PageTitle title="All Posts" />
+    <div class="container">
+      <PostPreview
+        class="post"
+        v-for="edge in $page.posts.edges"
+        v-bind:key="edge.id"
+        v-bind="edge.node"
+        :date="formatDate(edge.node.date)"
+      />
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  posts: allPost(sortBy: "date", order: DESC limit: 2) {
+    edges {
+      node {
+        title
+        path
+        lead
+        date
+        timeToRead
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import PageTitle from '~/components/PageTitle.vue'
+import { formattedDate } from "~/assets/js/utils.js";
+import PostPreview from "@/components/PostPreview.vue";
+
+export default {
+  components: {
+    PostPreview,
+    PageTitle
+  },
+  methods: {
+    formatDate(date) {
+      return formattedDate(date);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+section {
+  padding: $spacer;
+}
+
+.container {
+  margin-top: $spacer;
+
+  > * {
+    margin-bottom: $spacer;
+  }
+}
+</style>
